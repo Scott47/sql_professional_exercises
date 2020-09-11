6a8dbce6ab650abeac60fedb847be78c5b061c9e
scottsilver <scottsilver47@gmail.com>
2020-09-11 13:22:13 -0500

    stored procedures and triggers

--- book3.sql
+++ book3.sql
@@ -50,19 +50,29 @@ WHERE s.invoice_number = '7628231837';
 -- Returning a Vehicle
 -- Carnival would also like to handle the case for when a car gets returned by a customer.
 -- When this occurs they must add the car back to the inventory and mark the original sales record as returned = True.
+-- Selling a vehicle
 CREATE PROCEDURE remove_vehicle_from_inventory(vehicleId int)
 LANGUAGE plpgsql
-AS $$
-BEGIN
+    AS $$
+    BEGIN
 
-UPDATE vehicles vehicle
-SET is_sold = True
-WHERE v.vehicle_id = vehicleId
+    UPDATE vehicles v.
+    SET is_sold = True
+    WHERE v.vehicle_id = vehicleId
 
 END
-$$;
+    $$;
 
 CALL remove_vehicle_from_inventory(2)
+
+ALTER TABLE
+  vehicles
+ADD
+  COLUMN is_sold bool NOT NULL
+SET
+  DEFAULT false;
+
+
 -- Carnival staff are required to do an oil change on the returned car before putting it back on the sales floor.
 -- In our stored procedure, we must also log the oil change within the OilChangeLog table.
 
@@ -76,3 +86,46 @@ CREATE TABLE OilChangeLog (
 -- Use the story above and extract the requirements.
 -- Build two stored procedures for Selling a car and Returning a car.
 -- Be ready to share with your class or instructor your result.
+
+-- BOOK 3 CH 5 Intro to Triggers
+-- Practice: Carnival
+-- Create a trigger for when a new Sales record is added, set the purchase date to 3 days from the current date.
+CREATE FUNCTION set_pickup_date()
+  RETURNS TRIGGER
+  LANGUAGE PlPGSQL
+AS $$
+BEGIN
+  -- trigger function logic
+  UPDATE sales
+  SET pickup_date = NEW.purchase_date + integer '7'
+  WHERE sales.sale_id = NEW.sale_id;
+
+  RETURN NULL;
+END;
+$$
+
+CREATE TRIGGER new_sale_made
+  AFTER INSERT
+  ON sales
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_pickup_date();
+
+
+-- Create a trigger for updates to the Sales table. If the pickup date is on or before the purchase date,
+-- set the pickup date to 7 days after the purchase date. If the pickup date is after the purchase date
+-- but less than 7 days out from the purchase date, add 4 additional days to the pickup date.
+
+
+CREATE TRIGGER new_sale_made
+  AFTER INSERT
+  ON sales
+  FOR EACH ROW
+  WHEN NEW.pickup_date <= NEW.purchase_date
+  EXECUTE PROCEDURE set_pickup_date();
+
+CREATE TRIGGER new_sale_made
+  AFTER INSERT
+  ON sales
+  FOR EACH ROW
+  WHEN NEW.pickup_date > NEW.purchase_date AND NEW.pickup_date < NEW.purchase_date + 7
+  EXECUTE PROCEDURE set_pickup_date();
